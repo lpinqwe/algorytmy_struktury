c3db5d7e5084673311a2a8735cd85b6d2a09284f
lpinqwe <gorelikovva@gmail.com>
2024-03-11 15:12:37 +0100

    zadanie testowe 0

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+#HarelikauValadar11.03.2024
+def read_std_in():
+    buf = input()
+    buf=buf.split(" ")
+    return buf
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+def reverce_arr(arr):
+    if(len(arr)==1):
+        return arr
+    return arr[::-1]
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    arr=read_std_in()
+    arr=reverce_arr(arr)
+    print(*arr)
